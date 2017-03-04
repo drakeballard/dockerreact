982d42a7476413e4a78bf3e3603a10abeaecc903
Drake Ballard <drakeballard@gmail.com>
2017-03-03 19:20:51 -0600

    adding the server will listen for the following port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// dependencies
 let express = require('express')
 let path = require('path')
 let app = express()
@@ -11,4 +12,9 @@ app.use(express.static('public'))
 
 // Creates an endpoint which returns it to index.html page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+// The server will start
+sever.listen(port, () => console.log(`Server Started on the port ${port}`))
+
+
 
