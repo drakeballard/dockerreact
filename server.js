80232861039e0da4f7dfd5313b087328c158617c
Drake Ballard <drakeballard@gmail.com>
2017-03-03 19:18:53 -0600

    adding an endpoint in server.js that returns the info into index.html

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ let app = express()
 let server = require('http').Server(app)
 
 // Use the environment port of available, or use default to 3000
-
 let port = process.env.PORT || 3000
 
+// Serve static files from public
+app.use(express.static('public'))
+
+// Creates an endpoint which returns it to index.html page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
