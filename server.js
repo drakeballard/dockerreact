645a98ebad82907ab4adfa7663916c75cb8f258e
Drake Ballard <drakeballard@gmail.com>
2017-03-03 19:14:25 -0600

    adding localhost port 3000

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ let express = require('express')
 let path = require('path')
 let app = express()
 let server = require('http').Server(app)
+
+// Use the environment port of available, or use default to 3000
+
+let port = process.env.PORT || 3000
+
+
